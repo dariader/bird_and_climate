c14c2db5c52689045f2659045ef07e7d22165edb
dariader <dary.spb@gmail.com>
2022-10-20 21:21:27 +0300

    test run

--- src/app.py
+++ src/app.py
@@ -1,9 +1,10 @@
+import dash
 from dash import Dash, dcc, html, Input, Output
 import os
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
