54d2ba7ba904c391d4cba569908ab0283c47110f
dariader <dary.spb@gmail.com>
2022-10-20 21:25:41 +0300

    debug on the fly

--- src/app.py
+++ src/app.py
@@ -1,13 +1,12 @@
 import dash
 from dash import Dash, dcc, html, Input, Output
-import os
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
+print('1')
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
+print('2')
 server = app.server
-
+print('3')
 app.layout = html.Div([
     html.H2('Hello World'),
     dcc.Dropdown(['LA', 'NYC', 'MTL'],
@@ -17,12 +16,14 @@ app.layout = html.Div([
     html.Div(id='display-value')
 ])
 
+print('4')
 
 @app.callback(Output('display-value', 'children'),
               [Input('dropdown', 'value')])
 def display_value(value):
     return f'You have selected {value}'
 
+print('5')
 
 if __name__ == '__main__':
     app.run_server(debug=True)
