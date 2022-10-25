30c39bd83b64d0e023f6c0e750cbdbc277f03005
dariader <dary.spb@gmail.com>
2022-10-25 22:52:43 +0300

    set up CI/CD

---
+++ release.sh
@@ -0,0 +1,16 @@
+set -e
+
+IMAGE_ID=$(docker inspect ${HEROKU_REGISTRY_IMAGE} --format={{.Id}})
+
+curl --netrc -X PATCH https://api.heroku.com/apps/$HEROKU_APP_NAME/formation \
+  -d '{
+  "updates": [
+    {
+      "type": "web",
+      "docker_image": "'"$IMAGE_ID"'"
+    }
+  ]
+}' \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
+  -H "Authorization: Bearer ${HEROKU_AUTH_TOKEN}"
